b579a0162e52b11491df992299d173d01ca4d345
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-17 00:28:58 -0800

    Added style for home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 import stylesUrl from "~/styles/index.css";
 
@@ -7,5 +8,20 @@ export const links: LinksFunction = () => {
 };
 
 export default function IndexRoute() {
-  return <div><h2>Hello Index Route</h2></div>;
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span> <span>😜</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes" className="button" style={{marginTop:"150"}}>Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
