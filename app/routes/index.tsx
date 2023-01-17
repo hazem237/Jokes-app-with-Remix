fae70d6de2314685a3da1d1bc7c5532b2e84c667
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-16 19:15:48 -0800

    initilized the sysetm

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+     <h1>Test</h1>
+     <h1>Hello</h1>
+    </div>
+  );
+}
